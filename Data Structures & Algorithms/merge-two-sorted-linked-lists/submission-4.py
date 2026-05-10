442154a91688b1553d0981fb540e50b11849ad79
Joohan(Lucas) <joohan224@gmail.com>
2026-05-10 14:47:32 -0400

    Add: merge-two-sorted-linked-lists - submission-4

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-4.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # Recursion
+        # Part1. If reach to the end, append it.
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        # Part2. 
+        # 1->2->3
+        # 3->4->5
+        # 현재 l1의 다음은 mergeTwoLists에서 받은 l2가 되어야함.
+        # 그리고 caller에게 현재 나를 돌려줘야 caller의 next가 내가 됨.
+        if l1.val <= l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+
+        # """
+        # 재귀로 풀 때: head 결정 → 나머지 subproblem 위임
+        # """
+        # # Part 1: Base Case - 한쪽이 끝나면 나머지 그대로 붙이기
+        # if not l1:
+        #     return l2
+        # if not l2:
+        #     return l1
+        
+        # # Part 2 & 3: 작은 쪽이 head가 되고, 나머지는 재귀에 맡김 
+        # if l1.val < l2.val:
+        #     l1.next = self.mergeTwoLists(l1.next, l2)# "내 뒤는 네가 알아서 해"
+        #     return l1 # "난 head야"
+        # else:
+        #     l2.next = self.mergeTwoLists(l1, l2.next)
+        #     return l2
+        
