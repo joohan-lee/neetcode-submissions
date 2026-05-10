06336f00121b879b95661e1553a33e14e71a85e7
Joohan(Lucas) <joohan224@gmail.com>
2026-05-10 15:23:58 -0400

    Add: reorder-linked-list - submission-3

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-3.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        # Brute force
+        nodes = []
+        cur = head
+        while cur:
+            nodes.append(cur)
+            cur = cur.next
+        
+        i, j = 0, len(nodes) - 1
+        
+        while i < j:
+            tmp = nodes[i].next
+            nodes[i].next = nodes[j]
+            nodes[j].next = tmp
+            i += 1
+            j -= 1
+        nodes[i].next = None # 마지막 노드의 next를 None으로 끊어줘야함. 아니면 cycle 발생.
